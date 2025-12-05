15f2a7773008553a41769bc862fc5de781800dfe
Swati Jain <jswati12@gmail.com>
2025-12-05 06:46:23 +0100

    page object with new config file

--- PageObjects/POManager.js
+++ PageObjects/POManager.js
@@ -1,15 +1,30 @@
-const { LoginPage } = require('./Loginpage');
-const { MycartPage } = require('./MycartPage');
-const { OrdersPage } = require('./OrdersPage');
-const { DashboardPage } = require('./DashboardPage');
-const { CheckoutPage } = require('./CheckoutPage');
-const { OrdersummaryPage } = require('./OrdersummaryPage');
+
+
+import { LoginPage } from './Loginpage';
+import { MycartPage } from './MycartPage';
+import { OrdersPage } from './OrdersPage';
+import { DashboardPage } from './DashboardPage';
+import { CheckoutPage } from './CheckoutPage';
+import { OrdersummaryPage } from './OrdersummaryPage';  
 
 
 
-
+/**
+ * POManager class is responsible for managing page objects in the application.
+ * It initializes instances of various page objects and provides getter methods
+ * to access them.
+ * 
+ * @class POManager
+ * @param {Object} page - The Playwright page object used for navigation and interaction.
+ */
 export class POManager {
+    /**
+     * Creates an instance of POManager.
+     * 
+     * @param {Object} page - The Playwright page object.
+     */
     constructor(page) {
+        // Initialize page objects
         this.page = page;
         this.LoginPage = new LoginPage(this.page);
         this.MycartPage = new MycartPage(this.page);
@@ -19,29 +34,57 @@ export class POManager {
         this.OrdersummaryPage = new OrdersummaryPage(this.page);
     }
 
+    /**
+     * Returns the LoginPage object.
+     * 
+     * @returns {LoginPage} The LoginPage instance.
+     */
     getLoginPage() {
-
         return this.LoginPage;
     }
-    getMycartPage() {
 
+    /**
+     * Returns the MycartPage object.
+     * 
+     * @returns {MycartPage} The MycartPage instance.
+     */
+    getMycartPage() {
         return this.MycartPage;
     }
 
+    /**
+     * Returns the OrdersPage object.
+     * 
+     * @returns {OrdersPage} The OrdersPage instance.
+     */
     getOrdersPage() {
-
         return this.OrdersPage;
     }
-    getDashboardPage() {
 
+    /**
+     * Returns the DashboardPage object.
+     * 
+     * @returns {DashboardPage} The DashboardPage instance.
+     */
+    getDashboardPage() {
         return this.DashboardPage;
     }
-    getCheckoutPage() {
 
+    /**
+     * Returns the CheckoutPage object.
+     * 
+     * @returns {CheckoutPage} The CheckoutPage instance.
+     */
+    getCheckoutPage() {
         return this.CheckoutPage;
     }
-    getOrdersummaryPage() {
 
+    /**
+     * Returns the OrdersummaryPage object.
+     * 
+     * @returns {OrdersummaryPage} The OrdersummaryPage instance.
+     */
+    getOrdersummaryPage() {
         return this.OrdersummaryPage;
     }
 }
